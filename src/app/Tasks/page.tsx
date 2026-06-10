f7f67e63e426db9dba7ca292ab190539abd77f1a
xTff <mscott2003@gmail.com>
2026-06-09 23:04:36 -0400

    Initial Commit - Getting this file ready for adding the header

--- src/app/Tasks/page.tsx
+++ src/app/Tasks/page.tsx
@@ -8,7 +8,6 @@ export default function Tasks() {
             <Navbar />
             <div className="header-container">
                 <div className="header-card">
-                    <h1>Hello</h1>
                 </div>
             </div>
         </div>
