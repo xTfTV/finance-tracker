14d33eec7ea2cc44ba82d38263a95d90bb82e97e
xTff <mscott2003@gmail.com>
2026-06-13 12:17:10 -0400

    Adding part of the basic layout - need to work on the button design for next/prev

--- src/app/Tasks/page.tsx
+++ src/app/Tasks/page.tsx
@@ -8,6 +8,15 @@ export default function Tasks() {
             <Navbar />
             <div className="header-container">
                 <div className="header-card">
+                    <h1>Task Manager</h1>
+                    <p>Plan your day, keep track of tasks, and stay organized</p>
+                </div>
+            </div>
+            <div className="next-prev-container">
+                <div className="next-prev-card">
+                    <button>Prev</button>
+                    <h3>Day of the Week</h3>
+                    <button>Next</button>
                 </div>
             </div>
         </div>
